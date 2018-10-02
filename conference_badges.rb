165b5224fc779a15de03425d917d566d3baf4bd2
ktochow1 <ktochow1@binghamton.edu>
2018-10-02 18:17:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,21 +9,6 @@ def batch_badge_creator(array)
   arr
 end
 
-# def assign_rooms
-#   rooms = [1, 2, 3, 4, 5, 6, 7]
-#   bade_maker(name)
-#   #return list of room assignments: "Hello, name! You'll be assigned to room roomnumber!"
-  
-#   #batch_badge_creator(array)
-#   array = array.new
-#   array.each_with_index { |item, index| 
-#   array[item] = index
-#   }
-#   # names.each { |n| names << "Hello #{n}!" }
-#   # rooms = []
-#   # rooms.each { |r| rooms << "You'll be assigned to room #{room}!" }
-# end
-
  def assign_rooms(list_of_speaker_names)
       room_assignments_holder = []
       list_of_speaker_names.each_with_index { |name, room_number| room_assignments_holder << "Hello, #{name}! You'll be assigned to room #{room_number + 1}!"}
