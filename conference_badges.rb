331c9bc02754a6d5cc397b24c2fc5b9df57de706
ktochow1 <ktochow1@binghamton.edu>
2018-10-02 16:34:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,8 +11,8 @@ end
 
 def assign_rooms
   rooms = [1, 2, 3, 4, 5, 6, 7]
-  guests = []
-  #return list of room assignments: "Hello, name! Your room is roomnumber!"
+  bade_maker(name)
+  #return list of room assignments: "Hello, name! You'll be assigned to room roomnumber!"
   
   #batch_badge_creator(array)
   array = array.new
